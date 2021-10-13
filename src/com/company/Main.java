e1e490b225799806000b90b05f022101c7d78744
Evgeniy Kontcevoy <e.kontsevoy@foxford.ru>
2021-10-13 14:27:25 +0300

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        TreeSet<Integer> newList = new TreeSet<>();
+        
+        for (int i = 0; i < intList.size(); i++) {
+            if (intList.get(i) > 0 && intList.get(i) % 2 == 0) {
+                    newList.add(intList.get(i));
+                }
+            }
+            for (Integer s : newList) {
+                System.out.println(s);
+            }
+
+    }
+}
